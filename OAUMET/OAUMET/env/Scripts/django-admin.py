48ccfcc980589dab9ed37cba5c483e15cd3ca618
trinitysege <trinitysege@gmail.com>
2016-11-27 15:47:36 -0800

    Progress working on the code

---
+++ OAUMET/OAUMET/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\loveth\Desktop\OAU Project\OAU-MAT\OAUMET\OAUMET\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
